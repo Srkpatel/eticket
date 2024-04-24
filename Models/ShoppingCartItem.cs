54470354d441cb8c579653ea10def9e9f6ce6ca2
anusri11 <appanusri01@gmail.com>
2024-04-24 16:56:15 +0530

    Add project files.

---
+++ Models/ShoppingCartItem.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Movie Movie { get; set; }
+
+        [Range(0,1)]
+        [Required(ErrorMessage = "You can only purchase one movie")]
+        public int Amount { get; set; } = 1;
+
+
+        public string ShoppingCartId { get; set; }
+    }
+}
